db8271270f636bfdb0cf557ce4d040f28e8c6ae9
ZYHowell <zhuangyh@sjtu.edu.cn>
2022-12-02 03:36:06 +0000

    add skeleton of graph.py

--- needle/graph.py
+++ needle/graph.py
@@ -1,6 +1,8 @@
 from queue import Queue
+from typing import Sequence
 
 from needle.autograd import Op, Tensor, Value
+from needle.ops import make_tuple, tuple_get_item
 from needle.utils import OrderedSet
 
 
@@ -25,13 +27,18 @@ class Graph:
         self.add_user(val, user)
 
 
-class SubGraph(Op):
+class Fused(Op):
 
-    def __init__(self, graph):
+    def __init__(self, graph, arg_to_params):
         self.graph = graph
+        self.arg_to_params = arg_to_params
+
+    def exec_graph(self, *args):
+        pass
 
     def compute(self, *args):
-        pass
+        # TODO(hongyi): replace it by a fused kernel
+        self.exec_graph(*args)
 
 
 def build_graph_from_tensor(root: Tensor):
@@ -55,5 +62,24 @@ def build_graph_from_tensor(root: Tensor):
     return graph
 
 
-def pattern_matching_on_graph(graph):
+def build_graph_from_tensors(root: Sequence[Tensor]):
+    root = make_tuple(*root)
+    return build_graph_from_tensor(root)
+
+
+# Given a subgraph, replace it by a fused operator. Then replace all use
+def _replace_nodes_by_fused_op(graph):
     pass
+
+# A fused operator may have multiple outputs. We add a tuple operator and get
+# its elements as the original outputs
+def _add_tuple_and_get_elements():
+    pass
+
+# version 1: An elementwise unary op after any op
+def pattern_matching_unary(graph):
+    pass
+
+# version 2: A series of elementwise unary ops after any op
+
+# version 3: Add elementwise binary ops as well
